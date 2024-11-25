a95b4d217b07bfaa204dfbec13b6be962fedb8f5
DangNhan23 <ndnhan22@clc.fitus.edu.vn>
2024-11-25 23:02:05 +0700

    update hbs

--- HTML TICKET MOVIE/views/hdb.js
+++ HTML TICKET MOVIE/views/hdb.js
@@ -1,18 +1,22 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const handlebars = require('express-handlebars')
-const port = 3000
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
 
-app.use(express.static(path.join('../views/public')));
-//template engine
+// Set up Handlebars as the view engine
 app.engine('handlebars', handlebars.engine());
-app.set('view engine','handlebars')
-// app.set('views', path.join(__dirname, '.\\resources\\views'));
+app.set('view engine', 'handlebars');
 
-app.set('views', path.join(__dirname, 'views/views'));
-console.log('./views/public')
+app.use(express.static('../views/public'));
+// Set the correct path to the 'views' directory
+app.set('views', path.join(__dirname, 'views'));  // Ensure this is correct
 
+// Your route handler
 app.get('/', (req, res) => {
-  res.render('home')
-})
+  res.render('home');  // This will look for home.handlebars inside the 'views' directory
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
