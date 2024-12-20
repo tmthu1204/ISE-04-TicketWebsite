66b01dfe2932bb87872da99d643163da63c77a78
DangNhan23 <ndnhan22@clc.fitus.edu.vn>
2024-12-20 18:24:57 +0700

    Update login.js, regis.js, make popup for otp section

--- Movie Ticket Management/FE/js/Common/login.js
+++ Movie Ticket Management/FE/js/Common/login.js
@@ -1,35 +1,3 @@
-document.getElementById("forgotPasswordLink").addEventListener("click", function () {
-    document.getElementById("forgot_pass").style.display = "block"; // Hiển thị form nhập email
-    document.getElementById("forgotPasswordLink").style.display = "none"; // Ẩn liên kết "Quên mật khẩu?"
-});
-
-document.getElementById("sendpassBtn").addEventListener("click", function () {
-    const formData = new FormData();
-    formData.append("action", "sendpass");
-    formData.append("email", document.getElementById("Forgotemail").value); // Get email from input field
-    
-    // Send OTP verification request to PHP
-    fetch("../../BE/Common/forgot_password.php", {
-        method: "POST",
-        body: formData,
-    })
-        .then(response => response.json()) // Parse JSON response
-        .then(data => {
-            console.log(JSON.stringify(data));
-            if (data.success) {
-                alert(data.message); // Show success message
-                window.location.href = "login.html"; 
-                document.getElementById("forgot_pass").style.display = "none";
-            } else {
-                alert(data.message);
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error); 
-            alert("Có lỗi xảy ra. Vui lòng thử lại.");
-        });
-});
-
 document.getElementById('formLogin').addEventListener('submit', async function (event) {
     event.preventDefault();
 
